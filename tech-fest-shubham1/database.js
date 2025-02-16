a3ec9b88b17fd2b1786d2e41dd2fece9727f6540
Shubham07badgujar <shubhambadgujari076@gmail.com>
2025-02-16 15:37:32 +0530

    new project

---
+++ tech-fest-shubham1/database.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+// Use the correct connection string
+mongoose.connect('mongodb://127.0.0.1:27017/yourdb', { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((err) => {
+    console.error('DB Connection Failed:', err);
+  });
